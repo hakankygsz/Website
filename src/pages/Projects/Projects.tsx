e3a59b71fbafda54a588ee24716e554e9447565b
hakankygsz <hakankaygusuzone@gmail.com>
2025-02-12 15:19:56 +0300

    xsxs

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -14,36 +14,31 @@ const Projects = () => {
                 </p>
             </div>
 
-
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {ProjectData.map((project, index) => (
-                    <div key={index} className="border border-neutral-900 p-4 flex flex-col items-center justify-between rounded-lg">
-                        <ul className='flex items-center justify-between gap-2 pb-4'>
+                    <div key={index} className="border border-neutral-900 p-4 flex flex-col items-center justify-between rounded-lg min-h-[400px]">
+                        <ul className='flex items-center justify-center gap-2 pb-4 flex-wrap'>
                             {project.technologies.map((v, i) => (
                                 <li key={i} className='h-10 px-2 flex items-center justify-center rounded-lg hover:ring duration-300 cursor-pointer border border-neutral-950'>
                                     {v}
                                 </li>
                             ))}
                         </ul>
-                        <img src={project.bannerUrl} alt="banner" className='w-[600px] mb-2 rounded-lg' />
                         <h2 className="text-2xl font-semibold mb-2 text-center">{project.title}</h2>
-                        <p className="text-sm text-gray-400 mb-6 text-center">{project.description}</p>
-                        <ul className='flex items-center justify-between gap-4 pb-4'>
+                        <p className="text-sm text-gray-400 mb-6 text-center flex-grow">{project.description}</p>
+                        <ul className='flex items-center justify-center gap-4 pb-4'>
                             {project.links.map((v, i) => (
                                 <Link to={v.url} key={i} className='h-14 w-14 flex items-center justify-center rounded-lg text-2xl cursor-pointer border border-neutral-950 hover:ring duration-300'>
                                     {v.Logo}
                                 </Link>
                             ))}
                         </ul>
-                        <button className='py-4 px-16 border border-neutral-900 hover:ring duration-300 rounded-lg'>
+                        <button className='py-4 px-16 border border-neutral-900 hover:ring duration-300 rounded-lg mt-auto'>
                             Daha Fazla Bilgi
                         </button>
                     </div>
                 ))}
             </div>
-
-
-
         </div>
     );
 };
--- src/pages/Projects/ProjectsData.tsx
+++ src/pages/Projects/ProjectsData.tsx
@@ -2,7 +2,6 @@ import { FaGithub } from "react-icons/fa";
 
 const ProjectData = [
     {
-        bannerUrl: "https://placehold.co/600x400",
         title: "B2C",
         description: "B2C modeline dayalı bir e-ticaret platformunun tasarımı ve geliştirilmesi. Kullanıcı dostu bir arayüzle, ürünler ve ödeme süreçleri yönetilerek, online alışveriş deneyimi sağlanmıştır.",
         technologies: ["React", "Node.js", "MySQL"],
@@ -10,7 +9,6 @@ const ProjectData = [
         ]
     },
     {
-        bannerUrl: "https://placehold.co/600x400",
         title: "Sanal Sözlük",
         description: "Türk Dil Kurumu'nun (TDK) resmi sözlüğüne dayanan ve anlık kelime arama sonuçlarını kullanıcıya sunan bir arama motoru uygulaması. Kullanıcılar, istedikleri kelimeleri girerek tanımları hızlıca öğrenebilirler.",
         technologies: ["Python", "Pandas"],
@@ -24,7 +22,6 @@ const ProjectData = [
         ]
     },
     {
-        bannerUrl: "https://placehold.co/600x400",
         title: "Yapay Zeka Paketi",
         description: "Yüz tanıma, el tanıma, plaka tanıma gibi bilgisayarla görme ve yapay zeka uygulamalarını içeren bir yazılım paketi. Gelişmiş görüntü işleme teknikleri ile güvenlik ve otomasyon sistemlerine entegre edilebilir.",
         technologies: ["Python", "OpenCV"],
